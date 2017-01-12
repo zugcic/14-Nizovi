e2f9bdbd092b4138ee11c878315a013cc0ec3a2e
Stjepan Zugcic <stzugcic@vsite.hr>
2017-01-12 19:37:56 +0100

    Napravljena InicijalizacijaNiza

--- InicijalizacijaNiza/InicijalizacijaNiza.cs
+++ InicijalizacijaNiza/InicijalizacijaNiza.cs
@@ -21,32 +21,35 @@ namespace Vsite.CSharp
 
         public static void InicijalizacijaNizaVrijednosnogTipa()
         {
-            int[] nizBrojeva = null;  // deklaracija reference na niz
+			int[] nizBrojeva = new int[N];//ili int[] { 1,2,3};//ili =new int[3];  // deklaracija reference na niz
 
-            // TODO: nizBrojeva inicijalizirati kao niz duljine N u kojem su članovi neinicijalizirani
+            //  nizBrojeva inicijalizirati kao niz duljine N u kojem su članovi neinicijalizirani
 
             IspisČlanova("Niz neinicijaliziranih objekata tipa", nizBrojeva);
 
-            // TODO: Svakom pojedinom članu niza pridružiti vrijednost različite od nule. Pokrenuti program i provjeriti ispis.
-
-            IspisČlanova("Niz inicijaliziranih objekata tipa", nizBrojeva);
+			// Svakom pojedinom članu niza pridružiti vrijednost različite od nule. Pokrenuti program i provjeriti ispis.
+			nizBrojeva[0] = 12;
+			nizBrojeva[3] = 14;
+			nizBrojeva[2] = 5;
+			nizBrojeva[1] = 5;
+			IspisČlanova("Niz inicijaliziranih objekata tipa", nizBrojeva);
         }
 
         public static void InicijalizacijaNizaReferentnogTipa()
         {
-            Control[] nizKontrola = null;  // deklaracija reference na niz 
+            Control[] nizKontrola = new Control[N];  // deklaracija reference na niz 
 
-            // TODO: nizKontrola inicijalizirati kao niz duljine N u kojem su članovi neinicijalizirani
+            // nizKontrola inicijalizirati kao niz duljine N u kojem su članovi neinicijalizirani
 
             IspisČlanova("Niz neinicijaliziranih objekata tipa", nizKontrola);
 
-            // TODO: Otkomentirati donje naredbe, pokrenuti program i provjeriti ispis.
-            //nizKontrola[0] = new Button();
-            //nizKontrola[1] = new Form();
-            //nizKontrola[2] = nizKontrola[0];
-            //nizKontrola[3] = new TextBox();
+			// Otkomentirati donje naredbe, pokrenuti program i provjeriti ispis.
+			nizKontrola[0] = new Button();
+			nizKontrola[1] = new Form();
+			nizKontrola[2] = nizKontrola[0];
+			nizKontrola[3] = new TextBox();
 
-            IspisČlanova("Niz inicijaliziranih objekata tipa", nizKontrola);
+			IspisČlanova("Niz inicijaliziranih objekata tipa", nizKontrola);
         }
 
         static void Main(string[] args)
